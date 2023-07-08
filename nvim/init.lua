2910c94a4b282f8afeeda878bef45b497cedda5b
krzkro4122 <krolkrzysztof1999@gmail.com>
2023-07-08 13:21:46 +0200

    git_files > find_files

--- nvim/init.lua
+++ nvim/init.lua
@@ -212,7 +212,7 @@ pcall(require('telescope').load_extension, 'fzf')
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = '[ ] Find files' })
+vim.keymap.set('n', '<leader>f', require('telescope.builtin').git_files , { desc = '[ ] Find files recognised by git' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
